96c0ce596f77a51189239c43507ad3c5ff864750
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-03 11:22:11 +0200

    Cleanup

--- 24SOX/python/sum_charge.py
+++ 24SOX/python/sum_charge.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 names = 'nr       type  resnr residue  atom   cgnr     charge       mass  typeB    chargeB      massB'
 names = names.split()
-df = pd.read_csv('24SOX.itp', skiprows=9, skipfooter=100, delim_whitespace=True, names=names, engine='python')
+df = pd.read_csv('../24SOX.itp', skiprows=0, skipfooter=-1, delim_whitespace=True, names=names, engine='python')
 print(df)
 
 sum_charge = df['charge'].sum()
--- 24SOX/run.sh
+++ 24SOX/run.sh
@@ -5,9 +5,18 @@ NMOL=10
 
 cd run/
 cp ../topol.top topol.top
+
 gmx insert-molecules -ci ../24SOX.pdb -o box.gro -box $L $L $L -nmol $NMOL
 gmx solvate -cp box.gro -o box_water.gro -cs tip4p -p topol.top
+
 gmx grompp -f ../mdp/min.mdp -p topol.top -c box_water.gro
 gmx genion -s topol.tpr -o box_water.gro -p topol.top -neutral
+
 gmx grompp -c box_water.gro -p topol.top -f ../mdp/min.mdp -o min.tpr
 gmx mdrun -deffnm min
+
+gmx grompp -c box_water.gro -p topol.top -f ../mdp/eql.mdp -o eql.tpr
+gmx mdrun -deffnm eql
+
+gmx grompp -c box_water.gro -p topol.top -f ../mdp/prd.mdp -o prd.tpr
+gmx mdrun -deffnm prd
