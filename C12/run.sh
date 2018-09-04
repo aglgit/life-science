b9d9e44f12e6f4f0d42bbcf06ba1c185e3812f37
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-04 16:19:55 +0200

    Mo molecules mo problems

--- C12/run.sh
+++ C12/run.sh
@@ -1,14 +1,13 @@
 set -e
 
-L=10.0
-NMOL=10
+L=5.0
+NMOL=1
 
 mkdir -p run/
 cd run/
 cp ../topol.top topol.top
 
-gmx insert-molecules -ci ../ch3_thr_stripped.pdb -o box.gro -box $L $L $L -nmol 1
-gmx insert-molecules -ci ../24SOX.pdb -o box.gro -f box.gro -nmol $NMOL
+gmx insert-molecules -ci ../C12.pdb -o box.gro -box $L $L $L -nmol $NMOL
 gmx solvate -cp box.gro -o box_water.gro -cs tip4p -p topol.top
 
 gmx grompp -f ../mdp/min.mdp -p topol.top -c box_water.gro
