7345b79c34a5a5bba0951c3fdc6439a1e52edb7a
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-06 12:27:24 +0200

    System, but fix/freeze not working

--- System/run.sh
+++ System/run.sh
@@ -7,18 +7,20 @@ mkdir -p run/
 cd run/
 cp ../topol.top topol.top
 
-gmx insert-molecules -ci ../ch3_thr_stripped.pdb -o box.gro -box $L $L $L -nmol 1
+gmx editconf -f ../ch3_thr_nopep.pdb -o box.gro -c -d 1.0 -bt cubic
 gmx insert-molecules -ci ../../24SOX/24SOX.pdb -o box.gro -f box.gro -nmol $NMOL
-gmx solvate -cp box.gro -o box_water.gro -cs tip4p -p topol.top
 
-gmx grompp -f ../mdp/min.mdp -p topol.top -c box_water.gro
-gmx genion -s topol.tpr -o box_water.gro -p topol.top -neutral
+gmx grompp -f ../mdp/min.mdp -p topol.top -c box.gro
+gmx genion -s topol.tpr -o box.gro -p topol.top -neutral
 
-gmx grompp -c box_water.gro -p topol.top -f ../mdp/min.mdp -o min.tpr
-gmx mdrun -deffnm min
+gmx grompp -c box.gro -p topol.top -f ../mdp/min.mdp -o min.tpr
+gmx mdrun -deffnm min 
 
-gmx grompp -c box_water.gro -p topol.top -f ../mdp/eql.mdp -o eql.tpr
-gmx mdrun -deffnm eql
+gmx grompp -c box.gro -p topol.top -f ../mdp/eql.mdp -o eql.tpr
+gmx mdrun -deffnm eql 
 
-gmx grompp -c box_water.gro -p topol.top -f ../mdp/prd.mdp -o prd.tpr
-gmx mdrun -deffnm prd
+gmx grompp -c box.gro -p topol.top -f ../mdp/eql2.mdp -o eql.tpr
+gmx mdrun -deffnm eql 
+
+gmx grompp -c box.gro -p topol.top -f ../mdp/prd.mdp -o prd.tpr
+gmx mdrun -deffnm prd 
