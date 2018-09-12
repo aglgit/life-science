594ef1291be30e98d26163770abc588fa945427e
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-12 10:17:06 +0200

    Progressive

--- System/run.sh
+++ System/run.sh
@@ -5,7 +5,7 @@ cd run/
 cp ../topol.top topol.top
 cp ../index.ndx index.ndx
 
-gmx editconf -f ../system.pdb -o box.gro -box 4.33 4.50 6.5
+gmx editconf -f ../24SOX_C12.pdb -o box.gro -box 4.33 4.50 6.5
 
 gmx grompp -c box.gro -p topol.top -f ../mdp/min.mdp -o min.tpr -n index.ndx
 gmx mdrun -deffnm min
