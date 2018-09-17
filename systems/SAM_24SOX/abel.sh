5405efbf15caebcdb44c21de6220831d0ccd85df
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-17 13:00:31 +0200

    Clean

--- systems/SAM_24SOX/abel.sh
+++ systems/SAM_24SOX/abel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=SAM-1
+#SBATCH --job-name=CONF150
 #SBATCH --account=nn4654k
-#SBATCH --time=05:00:00
+#SBATCH --time=05:30:00
 #SBATCH --mem-per-cpu=2G
 #SBATCH --nodes=1 --ntasks-per-node=1
 
@@ -11,5 +11,5 @@ source /cluster/bin/jobsetup
 
 # Run job
 module load gromacs
-./run_conf.sh conf135
+./run_conf.sh conf150
 
---
+++ systems/SAM_24SOX/run_pull.sh
@@ -0,0 +1,12 @@
+set -e
+
+mkdir -p confs/
+cd run/
+
+gmx grompp -c pull_eql.gro -p topol.top -f ../mdp/pull_prd.mdp -o pull_prd.tpr -n index.ndx
+gmx mdrun -deffnm pull_prd
+
+echo 0 | gmx trjconv -s pull_prd.tpr -f pull_prd.xtc -o ../confs/conf.gro -sep
+
+exit 1
+
