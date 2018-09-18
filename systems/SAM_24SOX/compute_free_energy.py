9ec78f15d06e82a94e4594eabb1ef45c8a9156f7
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-18 13:22:36 +0200

    Minor mod

--- systems/SAM_24SOX/compute_free_energy.py
+++ systems/SAM_24SOX/compute_free_energy.py
@@ -5,10 +5,11 @@ import os
 import re
 from scipy.integrate import simps
 
-files = ['force/%s' % f for f in os.listdir('force')]
+files = ['force/%s' % f for f in os.listdir('force') if not f.startswith('.')]
 ordered_files = sorted(files, key=lambda x: (int(re.sub('\D','',x)),x))
 
 mean_force = np.zeros(len(ordered_files))
+std_force = np.zeros(len(ordered_files))
 
 for i, file in enumerate(ordered_files):
     with open(file, 'r') as infile:
@@ -18,11 +19,13 @@ for i, file in enumerate(ordered_files):
         names = ['time', 'force']
         df = pd.read_csv(infile, delim_whitespace=True, names=names)
         mean_force[i] = df['force'].mean()
+        std_force[i] = df['force'].std()
 
-files = ['pos/%s' % f for f in os.listdir('pos')]
+files = ['pos/%s' % f for f in os.listdir('pos') if not f.startswith('.')]
 ordered_files = sorted(files, key=lambda x: (int(re.sub('\D','',x)),x))
 
 mean_pos = np.zeros(len(ordered_files))
+std_pos = np.zeros(len(ordered_files))
 
 for i, file in enumerate(ordered_files):
     with open(file, 'r') as infile:
@@ -32,9 +35,13 @@ for i, file in enumerate(ordered_files):
         names = ['time', 'pos']
         df = pd.read_csv(infile, delim_whitespace=True, names=names)
         mean_pos[i] = df['pos'].mean()
+        std_pos[i] = df['pos'].std()
 
 
-plt.plot(mean_pos, mean_force)
+plt.errorbar(mean_pos, mean_force, xerr=None, yerr=std_force)
+plt.title(r'$\langle \frac{dA}{dx} \rangle$')
+plt.xlabel('distance from surface [nm]')
+plt.ylabel('restraint force [kJ/mol/nm]')
 plt.show()
 
 free_energy = simps(mean_force, mean_pos)
