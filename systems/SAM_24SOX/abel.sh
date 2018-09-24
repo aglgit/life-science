66ab1fb07f4ba62e3f6ddca99222256d5c475fdd
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-24 14:41:53 +0200

    Umbrella runs

--- systems/SAM_24SOX/abel.sh
+++ systems/SAM_24SOX/abel.sh
@@ -2,9 +2,10 @@
 
 #SBATCH --job-name=CONF
 #SBATCH --account=nn4654k
-#SBATCH --time=06:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mem-per-cpu=2G
-#SBATCH --nodes=1 --ntasks-per-node=4
+#SBATCH --nodes=1 --ntasks-per-node=16
+#SBATCH --output=log/slurm-CONF.out
 
 ## Set up job environment
 source /cluster/bin/jobsetup
--- systems/SAM_24SOX/run_abel.sh
+++ systems/SAM_24SOX/run_abel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+mkdir -p log/
 declare -a arr=("conf1" "conf10" "conf20" "conf30" "conf40" "conf50"
-                "conf60" "conf70" "conf90" "conf100")
+                "conf60" "conf70" "conf80" "conf90" "conf100"
+		"conf110" "conf120" "conf130" "conf140" "conf150")
 
 for i in "${arr[@]}"
 do
