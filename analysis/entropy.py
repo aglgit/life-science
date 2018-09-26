1054f9645b7fd6647d56a9069a5bc08523f2d622
Andreas Lefdalsnes <andreas.lefdalsnes93@gmail.com>
2018-09-26 15:59:59 +0200

    Analysis

---
+++ analysis/entropy.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+systems = ['24SOX', '25-OX', '27-OX']
+system_paths = ['../systems/SAM_%s/umbrella/clust-size.xvg' % s for s in systems]
+names = ['Cluster #', '# Structures']
+
+def read_xvg(filename, names):
+    with open(filename, 'r') as infile:
+        for line in infile:
+            if '@g0 type bar' in line:
+                break
+        df = pd.read_csv(infile, delim_whitespace=True, names=names)
+
+        return df
+
+for i, path in enumerate(system_paths):
+    df = read_xvg(path, names)
+    total = df[names[1]].sum()
+    p = df[names[1]].values / total
+    entropy = np.sum(-p*np.log(p))
+
+    plt.plot(df[names[0]], df[names[1]], label='%s, S=%.3f' % (systems[i], entropy))
+plt.xlabel(names[0])
+plt.ylabel(names[1])
+plt.title("Cluster analysis of molecules at surface")
+plt.legend()
+plt.show()
---
+++ analysis/free_energy.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+systems = ['24SOX', '25-OX', '27-OX']
+system_paths = ['../systems/SAM_%s/umbrella/hist.xvg' % s for s in systems]
+names = ['z [nm]', 'PMF [kJ/mol]']
+
+def read_xvg(filename, names):
+    with open(filename, 'r') as infile:
+        for line in infile:
+            if '@TYPE xy' in line:
+                break
+        df = pd.read_csv(infile, delim_whitespace=True, names=names)
+
+        return df
+
+zeros = [-46, -5, -4]
+for i, path in enumerate(system_paths):
+    df = read_xvg(path, names)
+    df[names[1]] -= zeros[i]
+    plt.plot(df[names[0]], df[names[1]], label=systems[i])
+
+plt.xlabel(names[0])
+plt.ylabel(names[1])
+plt.title('Potential of mean force')
+plt.legend()
+plt.show()
